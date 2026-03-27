fa3f8b435af843eadada0e2982c313a0f348dd70
jellyM0on <ylana.ong@gmail.com>
2026-03-28 02:02:02 +0800

    feat: add model with attention

---
+++ src/models/attention.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+
+@tf.keras.utils.register_keras_serializable()
+class AttentionPooling(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.score_dense = tf.keras.layers.Dense(1)
+
+    def call(self, x):
+        score = self.score_dense(x)
+        weights = tf.nn.softmax(score, axis=1)
+        return tf.reduce_sum(x * weights, axis=1)
+
+def build_attention_model(vectorizer, vocab_size, embedding_dim, lstm_units, dropout_rate, num_classes):
+    inputs = tf.keras.Input(shape=(1,), dtype=tf.string)
+    x = vectorizer(inputs)
+    x = tf.keras.layers.Embedding(
+        input_dim=vocab_size,
+        output_dim=embedding_dim,
+    )(x)
+    x = tf.keras.layers.Bidirectional(
+        tf.keras.layers.LSTM(lstm_units, return_sequences=True)
+    )(x)
+    x = tf.keras.layers.Dropout(dropout_rate)(x)
+    x = AttentionPooling()(x)
+    x = tf.keras.layers.Dense(64, activation="relu")(x)
+    x = tf.keras.layers.Dropout(dropout_rate)(x)
+    outputs = tf.keras.layers.Dense(num_classes, activation="sigmoid")(x)
+    return tf.keras.Model(inputs, outputs, name="goemotions_attention")
